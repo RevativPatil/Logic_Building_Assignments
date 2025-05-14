601fb3d9072cfc980a100472859cff935b307759
RevativPatil <revatip290@gmail.com>
2025-05-14 21:15:34 +0530

    Add files via upload

---
+++ Assignments_1/program2.c
@@ -0,0 +1,25 @@
+// program to divide two numbers
+
+///////////////////////////////////////////////////////////////////////////////
+// File Name   :  program1.c
+// Description :  
+// Autor       :  Revati Vinod Patil
+// Date        :  09/05/2025
+///////////////////////////////////////////////////////////////////////////////
+
+#include<stdio.h>
+
+void Display()
+{
+    int i = 0;
+    for(i =0 ; i <=4; i++)
+    {
+        printf("Marvellous \n");
+    }
+}
+
+int main()
+{
+    Display();
+    return 0;
+}
